2910d4c1d8e4bb3456b357ddb4052cc1d7ed328f
MashedPotats <samuelli4521@gmail.com>
2016-02-04 21:52:48 -0800

    2-4-2016
    The sliding up activity is halfway done... Will be moving to Lion

--- resources/js/controllers/MediaController.js
+++ resources/js/controllers/MediaController.js
@@ -2,6 +2,28 @@
  * Created by Samuel on 1/12/2016.
  */
 
-mainApp.controller("MediaController", ["$scope", function($scope) {
+mainApp.controller("MediaController", ["$scope", function ($scope) {
+    var iFrameContainer = document.querySelector('.media-iframe--container');
+    var iFrame = document.querySelector('.media-iframe--iframe');
 
-}]);
+    var listItems = document.querySelectorAll('.mm-list--item');
+    var listItemsLength = listItems.length;
+
+    for (var i = 0; i < listItemsLength; i++) {
+        listItems[i].addEventListener('click', function (e) {
+            e.preventDefault();
+
+            iFrameContainer.classList.add('ready');
+
+            iFrame.src = this.getAttribute('href');
+
+            function onFrameLoad() {
+                iFrameContainer.classList.add('open');
+
+                iFrame.removeEventListener('load', onFrameLoad);
+            }
+            iFrame.addEventListener('load', onFrameLoad);
+        });
+    }
+}])
+;
