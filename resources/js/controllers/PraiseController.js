b0e132b4620a2bc22e7c56279dc9ce3a58145408
samuel <samuelli4521@gmail.com>
2016-02-16 15:06:29 -0800

    Febuary 16, 2016 3:06 PM
    Created a start to the praise section of m4christ-mobile. Required components:
            Slide up thing from media.html
            A new list component
            Create/finish a very solid audio player w/ playlist
            Lots of html files for each category
    
     It's going to a lot of fun and mind blowing stuff in the future.
    
     FUTURE GOALS:
    
     Finish and clean up seminar
    
    Put it onto m4christ :P

---
+++ resources/js/controllers/PraiseController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by samuel on 2/16/16.
+ */
+
+mainApp.controller('PraiseController', ['$scope', function ($scope) {
+    $scope.CDVDDowns = ['風中傳愛歌本', '和風音樂詩歌 CD 光碟', '音樂卡拉ＯＫ視頻 DVD 光碟'];
+}]);
