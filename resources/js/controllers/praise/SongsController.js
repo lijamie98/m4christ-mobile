af550b120aba4568b1837d0fd7f242b77268aaf4
samuel <samuelli4521@gmail.com>
2016-03-15 22:20:12 -0700

    March 15, 2016 19:19 PM
    The 1-500 songs selector page is now finished.
    
     FUTURE GOALS:
    
     Finish and clean up seminar (NO)
    
     Just clean up everything and finish everything
    
    Put it onto m4christ :P (DONE! (at stage-01.m4christ.net))

---
+++ resources/js/controllers/praise/SongsController.js
@@ -0,0 +1,49 @@
+/**
+ * Created by samuel on 2/18/16.
+ */
+
+mainApp.controller('SongsController', ['$scope', '$http', function ($scope, $http) {
+    $scope.title = "耶穌是主 - 線上播放";
+
+    // http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+    String.prototype.format = function () {
+        var formatted = this;
+        for (var arg in arguments) {
+            formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+        }
+        return formatted;
+    };
+
+    if (window.self !== window.top)
+        window.parent.postMessage('openFrame', '*');
+    globalFrame.changeOpenOnMessage(true);
+
+    $scope.lists = (function() {
+        var songs = 500;
+        var lists = [];
+
+        var iterations = songs / 50;
+
+        for (var i = 1; i <= iterations; i++) {
+            var number = i * 50;
+            lists.push({
+                "start": number - 49,
+                "end" : number
+            });
+        }
+
+        return lists;
+    }());
+
+    var urlTemplate = '/mobile/praise/play.html?start={0}&end={1}';
+    $scope.open = function(start, end) {
+        globalFrame.open(urlTemplate.format(start, end));
+    };
+
+    $scope.close = function () {
+        if (window.self !== window.top)
+            window.parent.postMessage('closeFrame', '*');
+        else
+            window.location.href = '/mobile/praise.html';
+    };
+}]);
