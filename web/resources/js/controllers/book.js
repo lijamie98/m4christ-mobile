2f7f8f7675036f86cb3b69f50b49a14bc4b251bc
samuel <samuelli4521@gmail.com>
2016-07-19 19:04:16 -0700

    July 20, 2016 7:03 AM (i'm in taiwan)
    Created. A. Scrollbar for book.html.
    
    FUTURE GOALS:
    This is a big one, rewrite the site without angular. Should help with speed.
    Add video support to play.js. Will be part of the pure JS project

--- web/resources/js/controllers/book.js
+++ web/resources/js/controllers/book.js
@@ -12,19 +12,27 @@ window.addEventListener("load", function () {
         return formatted;
     };
 
-    var bookHTML = document.getElementById("skeleton").innerHTML;
+    var bookHTML;
+    var htmlXhttp = new XMLHttpRequest();
+    htmlXhttp.onreadystatechange = function () {
+        if (htmlXhttp.readyState == 4 && htmlXhttp.status == 200) {
+            bookHTML = htmlXhttp.responseText;
+        }
+    };
+    htmlXhttp.open("GET", "/resources/templates/product.html", true);
+    htmlXhttp.send();
 
     var dataObject;
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            dataObject = JSON.parse(xhttp.responseText);
+    var booksXhttp = new XMLHttpRequest();
+    booksXhttp.onreadystatechange = function () {
+        if (booksXhttp.readyState == 4 && booksXhttp.status == 200) {
+            dataObject = JSON.parse(booksXhttp.responseText);
             useData();
         }
     };
-    xhttp.open("GET", "http://m.m4christ.net/mobile/json/products/_filter/B", true);
-    xhttp.send();
+    booksXhttp.open("GET", "http://m.m4christ.net/mobile/json/products/_filter/B", true);
+    booksXhttp.send();
 
     var imgTemplateUrl = 'http://static.m4christ.net/products/{0}/{1}-ths.jpg';
     var mainGrid = document.getElementById("main-grid");
@@ -53,7 +61,80 @@ window.addEventListener("load", function () {
             document.querySelector(".mdl-layout__content").style.overflow = "auto";
         });
 
+        mainGrid.firstElementChild.firstElementChild.children[1].firstElementChild.addEventListener("load", function () {
+            updatePosition();
+        });
+
         setTimeout(function () {
         }, 500);
+    }
+
+    // the scroll thing
+
+    var mobile = (function () {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }());
+
+    var getPositionX;
+    (function () {
+        if (mobile) {
+            getPositionX = function (e) {
+                return e.touches[0].clientY;
+            };
+        }
+        else
+            getPositionX = function (e) {
+                return e.clientY;
+            };
+
+    }());
+
+    if (mobile) {
+        var mainLayout = document.querySelector(".mdl-layout__content");
+        var maScroll = document.querySelector(".ma-scroll");
+        var scrollBar = maScroll.querySelector(".ma-scroll-bar");
+        var maxScroll;
+
+        maScroll.addEventListener("mousedown", scrollStart);
+        maScroll.addEventListener("touchstart", scrollStart);
+        document.addEventListener("mouseup", scrollStop);
+        document.addEventListener("touchend", scrollStop);
+
+        function scrollStart(e) {
+            moveScroll(e)
+
+            document.addEventListener("mousemove", moveScroll);
+            document.addEventListener("touchmove", moveScroll);
+        }
+
+        function scrollStop() {
+            document.removeEventListener("mousemove", moveScroll);
+            document.removeEventListener("touchmove", moveScroll);
+        }
+
+        function moveScroll(e) {
+            var percentage = (getPositionX(e) - maScroll.getBoundingClientRect().top) / maScroll.offsetHeight;
+            if (percentage > 1) {
+                percentage = 1;
+            }
+            mainLayout.scrollTop = maxScroll * percentage;
+            console.log(maxScroll);
+            e.preventDefault();
+        }
+
+        mainLayout.addEventListener("scroll", updatePosition);
+
+        function updatePosition() {
+            maxScroll = mainLayout.scrollHeight - mainLayout.offsetHeight;
+            var value = ((mainLayout.scrollTop / maxScroll) * 100);
+
+            if (value > 100) {
+                value = 100;
+            }
+
+            scrollBar.style.top = "calc(" + value + "% - 24px)";
+        }
+    } else {
+        document.querySelector(".ma-scroll").style.display = "none";
     }
 });
